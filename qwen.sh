8ff764f963f3a2c0fdcf946107c750ad4fc97aa7
Patrick Leask <patrickaaleask@gmail.com>
2024-12-22 13:58:31 +0000

    modify qwen script to expect model activation to already exist

--- qwen.sh
+++ qwen.sh
@@ -10,6 +10,9 @@
 #SBATCH --mem 28G
 #SBATCH --array=0-47
 
+# Assumes that the model activations are already saved in the data directory.
+# Will probably OOM if not.
+
 module load cuda/11.3
 source /home3/wclv88/venv/bin/activate
 
@@ -23,15 +26,9 @@ MODEL_INDEX=$((INDEX / 24))
 LAYER=$((INDEX % 24))
 MODEL=${MODELS[$MODEL_INDEX]}
 
-if [ "$INDEX" -eq 0 ]; then
-  BATCH_SIZE=128
-else
-  BATCH_SIZE=1024
-fi
-
-echo "Running command for model: $MODEL, layer: $LAYER, batch_size: $BATCH_SIZE"
+echo "Running command for model: $MODEL, layer: $LAYER, batch_size: 1024"
 python3 train.py \
   --model "$MODEL" \
-  --batch_size "$BATCH_SIZE" \
+  --batch_size 1024 \
   --target_dict_size 50000 \
   --hook_name "blocks.$LAYER.hook_resid_pre"
